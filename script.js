21b49a2c421d9c9c6474ca84cd1287e96590f6f3
lucasgriffindev <66416659+LucasGriffinDev@users.noreply.github.com>
2023-05-05 14:06:57 +1000

    fixed form color status

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 const select = document.querySelector('#status-select');
-  select.addEventListener('change', () => {
-    const selectedOption = select.options[select.selectedIndex];
-    select.className = selectedOption.className;
-  });
-  
-    const button = document.getElementById('modal-open-button');
-    button.addEventListener('click', () => {
-      console.log('clicked');
-    });
-  });
-  
+select.addEventListener('change', () => {
+  const selectedOption = select.options[select.selectedIndex];
+  select.className = selectedOption.className + ' form-select';
+});
+
+const button = document.getElementById('modal-open-button');
+button.addEventListener('click', () => {
+  console.log('clicked');
+});
