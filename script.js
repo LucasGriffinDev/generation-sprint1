40b451cbaa0d64f8aa72a91f0cd0ea7909b42c5d
lucasgriffindev <66416659+LucasGriffinDev@users.noreply.github.com>
2023-05-05 13:53:05 +1000

    modal working

--- script.js
+++ script.js
@@ -1,27 +1,12 @@
-// get the modal element
-var modal = document.getElementById('myModal');
-let tasts = [];
-
-// get the button that opens the modal
-var btn = document.querySelector('#modal-open-button');
-
-// get the <span> element that closes the modal
-var span = document.getElementsByClassName('close')[0];
-
-// when the user clicks on the button, open the modal
-btn.onclick = function () {
-  console.log('clicked');
-  modal.style.display = 'block';
-};
-
-// when the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = 'none';
-};
-
-// when the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = 'none';
-  }
-};
+const select = document.querySelector('#status-select');
+  select.addEventListener('change', () => {
+    const selectedOption = select.options[select.selectedIndex];
+    select.className = selectedOption.className;
+  });
+  
+    const button = document.getElementById('modal-open-button');
+    button.addEventListener('click', () => {
+      console.log('clicked');
+    });
+  });
+  
