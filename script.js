00e8c58b4ebcb48b46224f472a662c49d02fab92
lucasgriffindev <66416659+LucasGriffinDev@users.noreply.github.com>
2023-05-07 18:05:31 +1000

    working final version

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const select = document.querySelector('#status-select');
 select.addEventListener('change', () => {
   const selectedOption = select.options[select.selectedIndex];
-  select.className = selectedOption.className + ' form-select';
-});
-
-const button = document.getElementById('modal-open-button');
-button.addEventListener('click', () => {
-  console.log('clicked');
+  if (selectedOption.className == 'bg-warning') {
+    select.className = selectedOption.className + ' form-select text-gray';
+  } else {
+    select.className = selectedOption.className + ' form-select text-white';
+  }
 });
