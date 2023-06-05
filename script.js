aae25a44c20bd9cd664d9bdeae8bf0394783a883
lucasgriffindev <66416659+LucasGriffinDev@users.noreply.github.com>
2023-06-06 09:40:22 +1000

    Revert "Merge branch 'main' into rowan-sprint2"
    This reverts commit 93991792c747bcb3f4f4a0a783af70284c8e4fd3, reversing
    changes made to eafe69fbff702189cd05a18b207b3d5ee9d6044e.

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
+// Customising modal contents
+var exampleModal = document.getElementById('myModal')
+exampleModal.addEventListener('show.bs.modal', function (event) {
+  let taskNum = event.relatedTarget.getAttribute('data-edit')
 
-// Selectors for the elements we'll be interacting with
+  if(taskNum.length > 0){
+    myModal.querySelector('.modal-title').textContent = 'Edit Task'
+    myModal.querySelector('#name').value = tasks[taskNum].name
+    myModal.querySelector('#assigned-select').value = tasks[taskNum].assignedTo
+    myModal.querySelector('#date').value = tasks[taskNum].dueDate
+    myModal.querySelector('#status-select').value = tasks[taskNum].status
+    myModal.querySelector('#status-select').dispatchEvent(new Event('change'))
+    myModal.querySelector('#description').value = tasks[taskNum].description
+  }
+})
+
 const select = document.querySelector('#status-select');
-const taskContainer = document.getElementById('task-container');
-const taskForm = document.querySelector('#task-form');
-const modalNameInput = document.querySelector('#name');
-const modalAssignedToInput = document.querySelector('#assigned-to');
-const modalDateInput = document.querySelector('#date');
-const modalStatusSelect = document.querySelector('#status-select');
-const modalDescriptionInput = document.querySelector('#description');
-let currentTaskIndex;
-
-// Event listener for select change
 select.addEventListener('change', () => {
   const selectedOption = select.options[select.selectedIndex];
   select.className = selectedOption.className + ' form-select';
@@ -21,199 +25,91 @@ select.addEventListener('change', () => {
   // }
 });
 
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
 
-// Event listener for form submission
-taskForm.addEventListener('submit', function (e) {
-  e.preventDefault();
-
-  let name = modalNameInput.value;
-  let assignedTo = modalAssignedToInput.value;
-  let date = modalDateInput.value;
-  let statusSelected = modalStatusSelect.value;
-  let description = modalDescriptionInput.value;
-
-  if (currentTaskIndex !== undefined) {
-    // If a task is being edited, update it
-    tasks[currentTaskIndex] = {
-      name: name,
-      assignedTo: assignedTo,
-      dueDate: date,
-      status: statusSelected,
-      description: description,
-    };
-
-    // Clear the currentTaskIndex
-    currentTaskIndex = undefined;
-  } else {
-    // If no task is being edited, create a new one
-    let newTask = {
-      name: name,
-      assignedTo: assignedTo,
-      dueDate: date,
-      status: statusSelected,
-      description: description,
-    };
-
-    tasks.push(newTask);
+//  json obect
+// rowan task
+submitTask = (taskNum = '') => {
+  let taskItem = {
+    name: document.getElementById('name').value,
+    assignedTo: document.getElementById('assigned-select').value,
+    dueDate: document.getElementById('date').value,
+    status: document.getElementById('status-select').value,
+    description: document.getElementById('description').value
   }
 
-  taskForm.reset();
-  renderTasks(tasks);
-  // close the modal
-  const modal = bootstrap.Modal.getInstance(document.getElementById('myModal'));
-  saveTasks();
-
-  modal.hide();
-});
-
-// Array of tasks
-let tasks;
-
-if (localStorage.getItem('tasks')) {
-  tasks = JSON.parse(localStorage.getItem('tasks'));
-} else {
-  tasks = [
-    {
-      name: 'Task 1',
-      assignedTo: 'John Doe',
-      dueDate: '2021-03-24',
-      status: 'IN PROGRESS',
-      description: 'This is a sample task.',
-    },
-    {
-      name: 'Task 2',
-      assignedTo: 'Jane Doe',
-      dueDate: '2021-01-24',
-      status: 'COMPLETED',
-      description: 'This is a sample task.',
-    },
-    {
-      name: 'Task 3',
-      assignedTo: 'Jane Doe',
-      dueDate: '2021-01-24',
-      status: 'REVIEW',
-      description: 'This is a sample task.',
-    },
-    {
-      name: 'Task 4',
-      assignedTo: 'Jane Doe',
-      dueDate: '2021-01-24',
-      status: 'NOT STARTED',
-      description: 'This is a sample task.',
-    },
-  ];
-}
-// Save tasks to local storage
-
-function saveTasks() {
-  localStorage.setItem('tasks', JSON.stringify(tasks));
+  if(typeof taskNum === 'number'){
+    tasks[taskNum] = taskItem
+  } else {tasks.push(taskItem)}
 }
 
-// Render tasks
-
-const getTaskStatusClass = (status) => {
-  switch (status) {
-    case 'IN PROGRESS':
-      return 'bg-warning';
-    case 'COMPLETED':
-      return 'bg-success';
-    case 'REVIEW':
-      return 'bg-primary';
-    case 'NOT STARTED':
-      return 'bg-danger';
-  }
+const taskJSON = {
+  name: 'Task 1',
+  assignedTo: 'John Doe',
+  dueDate: '2021-03-24',
+  status: 'In Progress',
+  description: 'This is a sample task.',
 };
 
-const renderTasks = (tasks) => {
-  taskContainer.innerHTML = '';
+//array of tasks
 
-  tasks.forEach(({ name, assignedTo, dueDate, status, description }) => {
-    taskContainer.innerHTML += `
-  <div class="table-responsive">
-        <table class="table table-bordered bg-light mt-2 rounded-lg">
-          <thead>
-            <tr>
-              <th class="col-1">Status</th>
-              <th class="col-2">Name</th>
-              <th class="col-2">Assigned to</th>
-              <th class="col-1">Due date</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                <div class="text-white text-center rounded-2 p-1 ${getTaskStatusClass(
-                  status
-                )}">
-                  ${status}
-                </div>
-              </td>
-              <td>${name}</td>
-              <td>
-                <div>${assignedTo}</div>
-              </td>
-              <td>
-                <p>${dueDate}</p>
-              </td>
-            </tr>
-            <tr>
-              <td class="fw-bold">
-                Description:
+const tasks = [
+  {
+    name: 'Wireframe',
+    assignedTo: 'Rowan',
+    dueDate: '2023-05-22',
+    status: 'done',
+    description: 'Finish the wireframe.',
+  },
+  {
+    name: 'Overall aesthetic',
+    assignedTo: 'Matt',
+    dueDate: '2023-05-22',
+    status: 'in progress',
+    description: 'Design and implement the overall aesthetic',
+  },
+  {
+    name: 'Navbar',
+    assignedTo: 'Rowan',
+    dueDate: '2023-05-22',
+    status: 'review',
+    description: 'design and implement navbar',
+  },
+  {
+    name: 'Task list',
+    assignedTo: 'Lucas',
+    dueDate: '2023-05-22',
+    status: 'todo',
+    description: 'write code for task lists',
+  },
+  {
+    name: 'Task modal',
+    assignedTo: 'Lucas',
+    dueDate: '2023-05-22',
+    status: 'done',
+    description: 'Design and implement code for new/edit task modal',
+  },
+];
 
-              </td>
-              <td colspan="2" class="description">
-                <span>${description}</span>
-              </td>
-              <td colspan="1">
-              <button class="btn btn-secondary float-end mx-2 edit-btn modal-open-button"
-              data-bs-toggle="modal"
-              data-bs-target="#myModal"
-              data-task-name="${name}" <!-- make sure this attribute is correctly assigned -->
-            Edit</button>
+//render tasks
 
-                  
-                </button>
-                <button class="btn btn-warning float-end delete-btn" data-task-name="${name}">Delete</button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      `;
-  });
+tasks.forEach((task) => {});
 
-};
-
-// Event listener for task container
-taskContainer.addEventListener('click', function (e) {
-  // Handle delete
-  if (e.target.classList.contains('delete-btn')) {
-    let taskName = e.target.dataset.taskName;
-
-
-    let index = tasks.findIndex((task) => task.name === taskName);
-    if (index !== -1) {
-      tasks.splice(index, 1);
-    }
-
-
-    renderTasks(tasks);
-  }
-  // Handle edit
-  if (e.target.classList.contains('edit-btn')) {
-    const taskName = e.target.dataset.taskName;
-    currentTaskIndex = tasks.findIndex((task) => task.name === taskName);
-
-    let task = tasks.find((task) => task.name === taskName);
-    if (task) {
-      modalNameInput.value = task.name;
-      modalAssignedToInput.value = task.assignedTo;
-      modalDateInput.value = task.dueDate;
-      modalStatusSelect.value = task.status;
-      modalDescriptionInput.value = task.description;
-    }
-    renderTasks(tasks);
-  }
-});
-
-renderTasks(tasks);
+// delete task
